784d4d0df22b4b5b3db2a27a85f864bca5952cd7
Carl James Llagas <llagas.carl24@gmail.com>
2026-05-01 02:18:56 +0800

    Module 1 complete - Property and Owner Management

--- resources/views/owners/index.blade.php
+++ resources/views/owners/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -13,6 +12,7 @@
             <th>Owner No</th>
             <th>First Name</th>
             <th>Last Name</th>
+            <th>Street</th>
             <th>City</th>
             <th>Tel No</th>
             <th>Actions</th>
@@ -24,9 +24,23 @@
             <td>{{ $owner->owner_no }}</td>
             <td>{{ $owner->f_name }}</td>
             <td>{{ $owner->l_name }}</td>
-            <td>{{ $owner->city }}</td>
-            <td>{{ $owner->tel_no }}</td>
+            <td>{{ $owner->street ?? 'N/A' }}</td>
+            <td>{{ $owner->city ?? 'N/A' }}</td>
+            <td>{{ $owner->tel_no ?? 'N/A' }}</td>
             <td>
-                <a href="{{ route('owners.edit', $owner->owner_no) }}" class="btn btn-sm btn-warning">Edit</a>
-                <form action="{{ route('owners.destroy', $owner->owner_no) }}" method="POST" class="d-inline">
+                <a href="{{ route('owners.edit', $owner->owner_no) }}" 
+                   class="btn btn-sm btn-warning">Edit</a>
+                <form action="{{ route('owners.destroy', $owner->owner_no) }}" 
+                      method="POST" class="d-inline">
                     @csrf @method('DELETE')
+                    <button onclick="return confirm('Delete this owner?')" 
+                            class="btn btn-sm btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @empty
+        <tr><td colspan="7" class="text-center">No owners found.</td></tr>
+        @endforelse
+    </tbody>
+</table>
+@endsection
